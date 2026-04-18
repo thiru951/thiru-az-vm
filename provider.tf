56d29a5341e65d9466bd6b41322940263f66fd3f
thiru951 <unvantedsw@gmail.com>
2026-04-19 02:05:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-  use_cli = false
 }
