cae3239971f98432cb3c11714626fbe94ff0facb
thiru951 <unvantedsw@gmail>
2026-04-19 21:58:50 +0530

    t1

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "thiru-rg"
+    resource_group_name  = "YOUR_STORAGE_RG"
     storage_account_name = "thiruterraformstate123"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
